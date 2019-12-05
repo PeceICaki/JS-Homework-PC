5e21f0ce5e636cf7938fd995432dd35fdc59fa21
PeceICaki <58535965+PeceICaki@users.noreply.github.com>
2019-12-05 01:14:08 +0100

    Add files via upload

---
+++ Input Exercise/js/script.js
@@ -0,0 +1,6 @@
+let x = prompt("Enter a Value", "0");
+let y = prompt("Enter a Value", "0");
+let num1 = parseInt(x);
+let num2 = parseInt(y);
+let sum = num1 + num2;
+console.log("The sum of", num1, "and", num2, "is: ", sum);
